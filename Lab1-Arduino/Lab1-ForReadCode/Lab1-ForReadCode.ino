e97866ca7f397f7438d686946ff325eff5683b2b
Frush <frushaj@rose-hulman.edu>
2025-12-08 00:46:46 -0500

    Initial Demo is working with this code. Need to impliment more advanced features now

--- Lab1-Arduino/Lab1-ForReadCode/Lab1-ForReadCode.ino
+++ Lab1-Arduino/Lab1-ForReadCode/Lab1-ForReadCode.ino
@@ -29,7 +29,7 @@ MultiStepper steppers;//create instance to control multiple steppers at the same
 
 #define stepperEnTrue false //variable for enabling stepper motor
 #define stepperEnFalse true //variable for disabling stepper motor
-#define max_speed 1500 //maximum stepper motor speed
+#define max_speed 1000 //maximum stepper motor speed
 #define max_accel 10000 //maximum motor acceleration
 
 int pauseTime = 2500;   //time before robot moves
@@ -44,6 +44,8 @@ const int rtEncoder = 19;        //right encoder pin (Mega Interrupt pins 2,3 18
 volatile long encoder[2] = {0, 0};  //interrupt variable to hold number of encoder counts (left, right)
 int lastSpeed[2] = {0, 0};          //variable to hold encoder speed (left, right)
 int accumTicks[2] = {0, 0};         //variable to hold accumulated ticks since last reset
+
+float currentAngle = 0;
 
 //function to set all stepper motor variables, outputs and LEDs
 void init_stepper(){
@@ -78,27 +80,34 @@ void init_stepper(){
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
-  // forward();
-  // delay(1000);
-  // reverse();
-  // delay(1000);
-  // pivot(1); //left
-  // delay(1000);
-  // pivot(0); //right
-  // delay(1000);
-  // turn(1); //turn left
-  // delay(1000);
-  // turn(0); //turn right
-  // delay(1000);
-  // spin(true); //left
-  // delay(1000);
-  // spin(false);
-  // delay(1000);
-  // stop();
-  int theta = 90;
-  goToAngle(theta);
+  //Initial Demo
 
+  reverse();
+  delay(1000);
+  forward();
+  delay(1000);
+  stop();
+  pivot(1); //left
+  delay(1000);
+  pivot(0); //right
+  delay(1000);
+  turn(1); //turn left
+  delay(1000);
+  turn(0); //turn right
+  delay(1000);
+  spin(true); //left
+  delay(1000);
+  spin(false);
+  delay(1000);
+  stop();
+  // float theta = 0;
+  // for (int i = 0; i < 360; i += 45){
+  //   goToAngle(i*1.0);
+  //   delay(1000);
+  // }
+  // goToAngle(90.0);
 }
 
 void loop() {
@@ -114,7 +123,7 @@ void forward() {
   digitalWrite(ltDirPin, HIGH); // Enables the motor to move in a particular direction
   digitalWrite(rtDirPin, HIGH); // Enables the motor to move in a particular direction
   // Makes 800 pulses for making one full cycle rotation
-  for (int x = 0; x < 3000; x++) {
+  for (int x = 0; x < 1500; x++) {
     digitalWrite(rtStepPin, HIGH);
     digitalWrite(ltStepPin, HIGH);
     delayMicroseconds(stepTime);
@@ -122,8 +131,6 @@ void forward() {
     digitalWrite(ltStepPin, LOW);
     delayMicroseconds(stepTime);
   }
-  // digitalWrite(rtStepPin, HIGH);
-  // digitalWrite(ltStepPin, HIGH);
 }
 
 void reverse(){
@@ -135,7 +142,7 @@ void reverse(){
   digitalWrite(ltDirPin, LOW); // Enables the motor to move in a particular direction
   digitalWrite(rtDirPin, LOW); // Enables the motor to move in a particular direction
   // Makes 800 pulses for making one full cycle rotation
-  for (int x = 0; x < 3000; x++) {
+  for (int x = 0; x < 1500; x++) {
     digitalWrite(rtStepPin, HIGH);
     digitalWrite(ltStepPin, HIGH);
     delayMicroseconds(stepTime);
@@ -148,16 +155,17 @@ void reverse(){
 
 // 1 for pivot left, 0 for pivot right
 void pivot(int direction){
-
+  int stepCount = 1012;
   if (direction ==  1){
-    for (int x = 0; x < 800; x++) {
+    for (int x = 0; x < stepCount; x++) {
       digitalWrite(rtStepPin, HIGH);
       delayMicroseconds(stepTime);
       digitalWrite(rtStepPin, LOW);
       delayMicroseconds(stepTime);
+      delayMicroseconds(1000);
     }
   } else {
-    for (int x = 0; x < 800; x++) {
+    for (int x = 0; x < stepCount; x++) {
       digitalWrite(ltStepPin, HIGH);
       delayMicroseconds(stepTime);
       digitalWrite(ltStepPin, LOW);
@@ -176,16 +184,13 @@ void turn(int direction){
 
   digitalWrite(ltDirPin, HIGH); // Enables the motor to move in a particular direction
   digitalWrite(rtDirPin, HIGH); // Enables the motor to move in a particular direction
-    if (direction == 1){
-    
-  }
  
   if (direction == 1){
-    for (int x = 0; x < 800; x++) {
+    for (int x = 0; x < 1212; x++) {
       digitalWrite(rtStepPin, HIGH);
       digitalWrite(ltStepPin, HIGH);
       delayMicroseconds(stepTime);
-      if (x % 2 ==0){
+      if (x % 8 ==0){
       digitalWrite(rtStepPin, LOW);
       }
       digitalWrite(ltStepPin, LOW);
@@ -193,11 +198,11 @@ void turn(int direction){
     } 
     // Makes 800 pulses for making one full cycle rotation
   } else {
-    for (int x = 0; x < 800; x++) {
+    for (int x = 0; x < 1212; x++) {
       digitalWrite(rtStepPin, HIGH);
       digitalWrite(ltStepPin, HIGH);
       delayMicroseconds(stepTime);
-      if (x % 2 ==0){
+      if (x % 8 ==0){
       digitalWrite(ltStepPin, LOW);
       }
       digitalWrite(rtStepPin, LOW);
@@ -244,34 +249,67 @@ void stop(){
 
 }
 
-void goToAngle(int thetag){ // degrees
-  digitalWrite(rtDirPin, HIGH);
-  digitalWrite(ltDirPin, HIGH);
-  thetac = 0;
-  if (thetag>180){
-    digitalWrite(rtDirPin, LOW);
-    thetag=thetag-180;
+// void goToAngle(float thetag){ // degrees
+//   digitalWrite(rtDirPin, HIGH);
+//   digitalWrite(ltDirPin, HIGH);
+//   float thetac = 0;
+//   if (thetag>180){
+//     digitalWrite(rtDirPin, LOW);
+//     thetag=thetag-180;
+//   } else {
+//     digitalWrite(ltDirPin, LOW);
+//   }
+//   while (thetac < thetag){
+//       digitalWrite(rtStepPin, HIGH);
+//       digitalWrite(ltStepPin, HIGH);
+//       delayMicroseconds(stepTime);
+//       digitalWrite(rtStepPin, LOW);
+//       digitalWrite(ltStepPin, LOW);
+//       delayMicroseconds(stepTime);
+//       thetac = thetac + .08895;
+//     }
+// }
+
+void goToAngle(float thetag) { // degrees
+  float thetac = currentAngle;
+
+  if (thetag > 180) {
+    digitalWrite(rtDirPin, LOW);  // reverse right wheel
+    digitalWrite(ltDirPin, HIGH); // left wheel forward
+    thetag -= 180;
   } else {
-    digitalWrite(ltDirPin, LOW);
+    digitalWrite(ltDirPin, LOW);  // reverse left wheel
+    digitalWrite(rtDirPin, HIGH); // right wheel forward
   }
+
+  // Now start turning
+  int step = 0;
+  int stepSpeed = 1000;
   while (thetac < thetag){
-    digitalWrite(rtStepPin, HIGH);
+      digitalWrite(rtStepPin, HIGH);
       digitalWrite(ltStepPin, HIGH);
       delayMicroseconds(stepTime);
       digitalWrite(rtStepPin, LOW);
       digitalWrite(ltStepPin, LOW);
       delayMicroseconds(stepTime);
-      thetac = thetac + .729;
-    }
+      thetac += 1530.0/8600.0;
+      step++;
+      Serial.println(step);
+      Serial.println(thetac);
+      delayMicroseconds(stepSpeed); //way to control speed of motors
+  }
+  currentAngle = thetac;
+
 }
 
-void goToGoalCm(int xg, yg){ // cm
+
+void goToGoalCm(float xg, float yg){ // cm
   digitalWrite(rtDirPin, HIGH);
   digitalWrite(ltDirPin, HIGH);
-  xc = 0;
-  yc = 0;
-  dc=0;
-  thetag = atan(xg/yg);
+  float xc = 0;
+  float yc = 0;
+  float dc=0;
+  float thetag = atan(xg/yg);
   goToAngle(thetag);
   float dg = sqrt((xc-xg)*(xc-xg)+(yc-yg)*(yc-yg));
   while (dg>dc){
@@ -285,7 +323,7 @@ void goToGoalCm(int xg, yg){ // cm
   }
 }
 
-void goToGoalIn(int xg, int yg){ // in
+void goToGoalIn(float xg, float yg){ // in
   float xgcm=xg/2.54;
   float ygcm=yg/2.54;
   goToGoalCm(xgcm,ygcm);
