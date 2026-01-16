6d07ba9c85de9787d809adef2dba54c8fdbdded3
Frush <frushaj@rose-hulman.edu>
2026-01-16 10:44:25 -0500

    Finished comments on M4

--- Lab2-Arduino/Unimate-Lab02/Unimate-Lab02-M4/Unimate-Lab02-M4.ino
+++ Lab2-Arduino/Unimate-Lab02/Unimate-Lab02-M4/Unimate-Lab02-M4.ino
@@ -1,4 +1,16 @@
-// M4
+/************************************
+   Unimate-Lab02-M4.ino
+   Andrew Frush, Val Rumzis, 1.16.25
+   ***********************************
+   The following program is an implimentation of lidar and sensor reading to be used in conjunction with the corresponding M7 code.
+   
+   The primary functions created are:
+   read_lidar: Given a lidar pin location, returns a distance in cm. 
+   read_sonar: Given a sonar echo and trig pin location, returns a distance in cm.
+   lidarRead: Getter method, callable from M7 to return lidar reading
+
+************************************/
+
 #include <Arduino.h>
 #include <RPC.h>
 
@@ -17,8 +29,6 @@
 #define ylwLED 7
 #define redLED 5
 
-#define tooClose 5 // collide distance in [cm]
-
 // Data structure for lidar
 struct lidar {
   int front;
@@ -31,17 +41,18 @@ struct lidar {
   MSGPACK_DEFINE_ARRAY(front, back, left, right, sonarLeft, sonarRight);
 } dist{};
 
+
 void setup() {
   Serial.begin(115200);
 
-  if (RPC.cpu_id() == CM7_CPUID) {
+  if (RPC.cpu_id() == CM7_CPUID) { //Sets up RPC for multicore
     blink(ylwLED, 100);
   } else {
     blink(redLED, 100);
   }
 
-  init_sensors();
-  RPC.bind("lidarRead", lidarRead);
+  init_sensors(); //initialize all sensors
+  RPC.bind("lidarRead", lidarRead); //Bind the lidarRead method to be callable from M7
 }
 
 void loop() {
@@ -53,6 +64,7 @@ void loop() {
   int tempRightSonar = 0;
   int tempLeftSonar = 0;
 
+  //Samples the sensors N times and averages their readings
   for (int i = 0; i < numSamples; i++) {
     tempRightLidar += read_lidar(rightLdr);
     tempLeftLidar += read_lidar(leftLdr);
@@ -62,13 +74,14 @@ void loop() {
     tempLeftSonar += read_sonar(leftSnrTrig, leftSnrEcho);
   }
 
-
   dist.front = tempFrontLidar/numSamples;
   dist.back = tempBackLidar/numSamples;
   dist.left = tempLeftLidar/numSamples;
   dist.right = tempRightLidar/numSamples;
   dist.sonarLeft = tempLeftSonar/numSamples;
   dist.sonarRight = tempRightSonar/numSamples;
+
+  //IGNORE - This code left for example later
 
   // if ((dist.front <= tooClose && dist.front != 0) || (dist.back <= tooClose && dist.back != 0) || (dist.left <= tooClose && dist.left != 0) || (dist.right <= tooClose && dist.right != 0)){ //this is smart but I don't think it can stay, this would be active for all states not just when it's colide
   //   RPC.call("collide");
@@ -79,8 +92,12 @@ void loop() {
   delay(10);  // Small delay to prevent overwhelming the sensors
 }
 
+/*
+  Ensures: This method is universal for M4 and M7, blinks LED to know which core is initializing and calls RPC.Begin
+           Useful for troubleshooting purposes.
+*/
 void blink(int led, int delaySeconds) {
-  // pinMode(led, OUTPUT);
+  // pinMode(led, OUTPUT); //Commented OUT to speed up startup
   // for (int i = 0; i < 10; i++) {
   //   digitalWrite(led, LOW);
   //   delay(delaySeconds);
@@ -91,10 +108,17 @@ void blink(int led, int delaySeconds) {
   digitalWrite(led, LOW);
 }
 
+/*
+  Ensures: This is a getter method. Returns the sensor data when called. Callable from M7.
+*/
 lidar lidarRead() {
   return dist;
 }
 
+/*
+  Ensures: Given lidar method.
+  Return: Int distance from lidar sensor in [cm]
+*/
 int read_lidar(int pin) {
   int d;
   int16_t t = pulseIn(pin, HIGH);
@@ -105,6 +129,9 @@ int read_lidar(int pin) {
   return d;
 }
 
+/*
+  Ensures: Initializes all the pins for the sensors (lidar and sonar).
+*/
 void init_sensors() {
   pinMode(frontLdr, INPUT);
   pinMode(backLdr, INPUT);
@@ -116,9 +143,12 @@ void init_sensors() {
   pinMode(rightSnrTrig, OUTPUT);
 }
 
+/*
+  Ensures: Reads sonar given the trig and echo pin
+  Return: Int sonar distance reading [cm]
+*/
 int read_sonar(int trig, int echo) {
-  // float velocity((331.5 + 0.6 * (float)(20)) * 100 / 1000000.0);
-  uint16_t distance, pulseWidthUs;
+    uint16_t distance, pulseWidthUs;
   // pinMode(trig, OUTPUT);
   digitalWrite(trig, LOW);
   digitalWrite(trig, HIGH);            //Set the trig pin High
@@ -126,8 +156,7 @@ int read_sonar(int trig, int echo) {
   digitalWrite(trig, LOW);             //Set the trig pin Low
   // pinMode(echo, INPUT);                //Set the pin to input mode
   pulseWidthUs = pulseIn(echo, HIGH);  //Detect the high level time on the echo pin, the output high level time represents the ultrasonic flight time (unit: us)
-  // distance = pulseWidthUs * velocity / 2.0;
-  distance = pulseWidthUs / 58;
+    distance = pulseWidthUs / 58;
   if (distance < 0 || distance > 30) { distance = 0; }
   return distance;
 }
