9efeed80b4e13857bb9f3a958d475314615a52df
rhit-rumzissc <rumzissc@rose-hulman.edu>
2026-01-20 09:16:38 -0500

    Update and rename Unimate-Lab3.ino to Unimate-Lab3-M7.ino

---
+++ Lab3-Arduino/Unimate-Lab3/Unimate-Lab3-M7.ino
@@ -0,0 +1,18 @@
+/************************************
+   Unimate-Lab3-M7.ino
+   Andrew Frush, Val Rumzis, 1.16.25
+   ***********************************
+   The following program is an implimentation of wall following behavior to avoid and go around obstacles to go to a goal. 
+   The primary functions created are:
+   followWall();
+
+  ************************************/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
