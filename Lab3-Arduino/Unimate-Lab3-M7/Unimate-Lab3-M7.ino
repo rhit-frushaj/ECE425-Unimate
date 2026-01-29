870106c6e0fc58cc262262631e77ce13ecb212f4
rhit-rumzissc <rumzissc@rose-hulman.edu>
2026-01-29 18:15:41 -0500

    added outer corner following

--- Lab3-Arduino/Unimate-Lab3-M7/Unimate-Lab3-M7.ino
+++ Lab3-Arduino/Unimate-Lab3-M7/Unimate-Lab3-M7.ino
@@ -45,9 +45,9 @@ int wait_time = 1000;  //delay for printing data
 
 bool running = true;  // global variable to keep whether the robot should be running or not, controls blocking
 
-bool lastSeenWall = true; // global variable for wall follow to track what wall was lost to ensure proper corner following. true = right & false = left
+bool lastSeenWall = true;  // global variable for wall follow to track what wall was lost to ensure proper corner following. true = right & false = left
 
-int state = 0; //Main State Controll Variable for the State Machine
+int state = 0;  //Main State Controll Variable for the State Machine
 // 0: Random Wander
 // 1: Follow Wall
 // 2: Follow Center
@@ -79,19 +79,18 @@ void loop() {
   dist = RPC.call("lidarRead").as<sensors>();  //get sensor data
   running = RPC.call("isRunning").as<bool>();
   if (running) {
-  digitalWrite(redLED, 0);
-  digitalWrite(ylwLED, 1);
-  digitalWrite(grnLED, 0);
+    digitalWrite(redLED, 0);
+    digitalWrite(ylwLED, 1);
+    digitalWrite(grnLED, 0);
   } else {
-  digitalWrite(redLED, 1);
-  digitalWrite(ylwLED, 0);
-  digitalWrite(grnLED, 0);
+    digitalWrite(redLED, 1);
+    digitalWrite(ylwLED, 0);
+    digitalWrite(grnLED, 0);
   }
-  // followWallOuterCorner();
+  followWallOuterCorner();
   //delay(50);
 
-  followCenterLogic();
-
+  //followCenterLogic();
 }
 
 //function to set all stepper motor variables, outputs and LEDs
@@ -268,6 +267,32 @@ void goToAngle(float thetag) {  // degrees
 }
 
 /*
+  Ensures: Moves the robot backwards by rotating the wheels backwards for some distance in cm. 
+  floast distance: Distance in cm to move backwards. Input should be positive.
+*/
+void reverse(float distance) {
+
+  Serial.println("forward function");
+  digitalWrite(redLED, HIGH);   //turn on red LED
+  digitalWrite(grnLED, LOW);    //turn off green LED
+  digitalWrite(ylwLED, LOW);    //turn off yellow LED
+  digitalWrite(ltDirPin, LOW);  // Enables the motor to move in a particular direction
+  digitalWrite(rtDirPin, LOW);  // Enables the motor to move in a particular direction
+
+  int steps = (int)ceil((distance * 800.0) / (8.5 * PI));  //this converts based on wheel diameter to steps from distance in cm
+  //Steps both motors backwards for the num steps calculated for the input distance
+  for (int x = 0; x < steps; x++) {
+    if (!running) { return; }
+    digitalWrite(rtStepPin, HIGH);
+    digitalWrite(ltStepPin, HIGH);
+    delayMicroseconds(stepTime);
+    digitalWrite(rtStepPin, LOW);
+    digitalWrite(ltStepPin, LOW);
+    delayMicroseconds(stepTime);
+  }
+}
+
+/*
   Ensures: Moves the robot to a position based on it's current position in centimeters. First rotates the robot then moves in a straight line directly to the goal. 
            The coordinate system of the robot can be visualized below:
 
@@ -321,9 +346,9 @@ void goToGoalCm(float xg, float yg) {  // cm
 */
 void followWall() {
   if (!running) {
-      Serial.println("collide followWall");
-      return;
-    }
+    Serial.println("collide followWall");
+    return;
+  }
   Serial.println(
     String("Sensor Values (Front, Back, Left, Right): ") + dist.front + ", " + dist.back + ", " + dist.left + ", " + dist.right);
   float speed = max_speed - 1100;
@@ -333,58 +358,58 @@ void followWall() {
   int kp = 15;
   int error = 0;
   // left wall follow
-   if ((dist.left > 0) && (dist.right == 0)){
-  if (dist.left > upperMidBand){  //for robot too far from wall
-    error = dist.left - (upperMidBand+lowerMidBand)/2;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
+  if ((dist.left > 0) && (dist.right == 0)) {
+    if (dist.left > upperMidBand) {  //for robot too far from wall
+      error = dist.left - (upperMidBand + lowerMidBand) / 2;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperRight.setSpeed(-1.0 * otherSpeed);
+      stepperLeft.setSpeed(-1.0 * speed);
+      Serial.println("Right Speed" + String(speed));
+      Serial.println("Left Speed" + String(otherSpeed));
+    } else if ((dist.left < lowerMidBand) && (dist.left > 0)) {  // robot too close to wall
+      error = (upperMidBand + lowerMidBand) / 2 - dist.left;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperRight.setSpeed(-1.0 * speed);
+      stepperLeft.setSpeed(-1.0 * otherSpeed);
+      Serial.println("Left Speed" + String(speed));
+      Serial.println("Right Speed" + String(otherSpeed));
     }
-    stepperRight.setSpeed(-1.0 * otherSpeed);
-    stepperLeft.setSpeed(-1.0 * speed);
-    Serial.println("Right Speed"+ String(speed));
-    Serial.println("Left Speed"+String(otherSpeed));
-  } else if ((dist.left < lowerMidBand) && (dist.left > 0)){ // robot too close to wall
-    error = (upperMidBand+lowerMidBand)/2 - dist.left;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
+  } else if ((dist.right > 0) && (dist.left == 0)) {
+    if (dist.right > upperMidBand) {  //for robot too far from wall
+      error = dist.right - (upperMidBand + lowerMidBand) / 2;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperLeft.setSpeed(-1.0 * otherSpeed);
+      stepperRight.setSpeed(-1.0 * speed);
+      Serial.println("Left Speed" + String(speed));
+      Serial.println("Right Speed" + String(otherSpeed));
+    } else if ((dist.right < lowerMidBand) && (dist.right > 0)) {  // robot too close to wall
+      error = (upperMidBand + lowerMidBand) / 2 - dist.right;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperLeft.setSpeed(-1.0 * speed);
+      stepperRight.setSpeed(-1.0 * otherSpeed);
+      Serial.println("Right Speed" + String(speed));
+      Serial.println("Left Speed" + String(otherSpeed));
     }
-    stepperRight.setSpeed(-1.0 * speed);
-    stepperLeft.setSpeed(-1.0 * otherSpeed);
-    Serial.println("Left Speed"+String(speed));
-    Serial.println("Right Speed"+String(otherSpeed));
   }
-   } else if ((dist.right > 0) && (dist.left == 0)){
-      if (dist.right > upperMidBand){  //for robot too far from wall
-    error = dist.right - (upperMidBand+lowerMidBand)/2;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
-    }
-    stepperLeft.setSpeed(-1.0 * otherSpeed);
-    stepperRight.setSpeed(-1.0 * speed);
-    Serial.println("Left Speed"+ String(speed));
-    Serial.println("Right Speed"+String(otherSpeed));
-  } else if ((dist.right < lowerMidBand) && (dist.right > 0)){ // robot too close to wall
-    error = (upperMidBand+lowerMidBand)/2 - dist.right;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
-    }
-    stepperLeft.setSpeed(-1.0 * speed);
-    stepperRight.setSpeed(-1.0 * otherSpeed);
-    Serial.println("Right Speed"+String(speed));
-    Serial.println("Left Speed"+String(otherSpeed));
-  }
-   }
   for (int i = 0; i < 5; i++) {
-      if (!running) {
+    if (!running) {
       Serial.println("collide followWall");
       return;
     }
-  stepperLeft.runSpeed();
-  stepperRight.runSpeed();
+    stepperLeft.runSpeed();
+    stepperRight.runSpeed();
   }
 }
 
@@ -394,9 +419,9 @@ void followWall() {
 */
 void followWallOuterCorner() {
   if (!running) {
-      Serial.println("collide followWall");
-      return;
-    }
+    Serial.println("collide followWallCorner");
+    return;
+  }
   Serial.println(
     String("Sensor Values (Front, Back, Left, Right): ") + dist.front + ", " + dist.back + ", " + dist.left + ", " + dist.right);
   float speed = max_speed - 1100;
@@ -406,70 +431,73 @@ void followWallOuterCorner() {
   int kp = 15;
   int error = 0;
   // left wall follow
-   if ((dist.left > 0) && (dist.right == 0)){
-    lastSeenWall = false; 
-  if (dist.left > upperMidBand){  //for robot too far from wall
-    error = dist.left - (upperMidBand+lowerMidBand)/2;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
+  if ((dist.left > 0) && (dist.right == 0)) {
+    lastSeenWall = false;
+    if (dist.left > upperMidBand) {  //for robot too far from wall
+      error = dist.left - (upperMidBand + lowerMidBand) / 2;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperRight.setSpeed(-1.0 * otherSpeed);
+      stepperLeft.setSpeed(-1.0 * speed);
+      Serial.println("Right Speed" + String(speed));
+      Serial.println("Left Speed" + String(otherSpeed));
+    } else if ((dist.left < lowerMidBand) && (dist.left > 0)) {  // robot too close to wall
+      error = (upperMidBand + lowerMidBand) / 2 - dist.left;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperRight.setSpeed(-1.0 * speed);
+      stepperLeft.setSpeed(-1.0 * otherSpeed);
+      Serial.println("Left Speed" + String(speed));
+      Serial.println("Right Speed" + String(otherSpeed));
     }
-    stepperRight.setSpeed(-1.0 * otherSpeed);
-    stepperLeft.setSpeed(-1.0 * speed);
-    Serial.println("Right Speed"+ String(speed));
-    Serial.println("Left Speed"+String(otherSpeed));
-  } else if ((dist.left < lowerMidBand) && (dist.left > 0)){ // robot too close to wall
-    error = (upperMidBand+lowerMidBand)/2 - dist.left;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
+  } else if ((dist.right > 0) && (dist.left == 0)) {
+    lastSeenWall = true;
+    if (dist.right > upperMidBand) {  //for robot too far from wall
+      error = dist.right - (upperMidBand + lowerMidBand) / 2;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperLeft.setSpeed(-1.0 * otherSpeed);
+      stepperRight.setSpeed(-1.0 * speed);
+      Serial.println("Left Speed" + String(speed));
+      Serial.println("Right Speed" + String(otherSpeed));
+    } else if ((dist.right < lowerMidBand) && (dist.right > 0)) {  // robot too close to wall
+      error = (upperMidBand + lowerMidBand) / 2 - dist.right;
+      otherSpeed = speed - kp * error;
+      if (otherSpeed < 0) {
+        otherSpeed = 0;
+      }
+      stepperLeft.setSpeed(-1.0 * speed);
+      stepperRight.setSpeed(-1.0 * otherSpeed);
+      Serial.println("Right Speed" + String(speed));
+      Serial.println("Left Speed" + String(otherSpeed));
     }
-    stepperRight.setSpeed(-1.0 * speed);
-    stepperLeft.setSpeed(-1.0 * otherSpeed);
-    Serial.println("Left Speed"+String(speed));
-    Serial.println("Right Speed"+String(otherSpeed));
+  } else if (lastSeenWall && (dist.right == 0)) {  // lost right wall
+    reverse(20);
+    goToAngle(270);
+    reverse(10);
+    return;
+  } else if ((!lastSeenWall) && (dist.left == 0)){  // lost left wall
+    reverse(20);
+  goToAngle(90);
+  reverse(10);
+  return;
+} else { // if detects obstacle in both exits corner follow
+return;
+}
+for (int i = 0; i < 5; i++) {
+  if (!running) {
+    Serial.println("collide followWall");
+    return;
   }
-   } else if ((dist.right > 0) && (dist.left == 0)){
-      lastSeenWall = true;
-      if (dist.right > upperMidBand){  //for robot too far from wall
-    error = dist.right - (upperMidBand+lowerMidBand)/2;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
-    }
-    stepperLeft.setSpeed(-1.0 * otherSpeed);
-    stepperRight.setSpeed(-1.0 * speed);
-    Serial.println("Left Speed"+ String(speed));
-    Serial.println("Right Speed"+String(otherSpeed));
-  } else if ((dist.right < lowerMidBand) && (dist.right > 0)){ // robot too close to wall
-    error = (upperMidBand+lowerMidBand)/2 - dist.right;
-    otherSpeed = speed - kp*error;
-    if (otherSpeed < 0){
-      otherSpeed = 0;
-    }
-    stepperLeft.setSpeed(-1.0 * speed);
-    stepperRight.setSpeed(-1.0 * otherSpeed);
-    Serial.println("Right Speed"+String(speed));
-    Serial.println("Left Speed"+String(otherSpeed));
-  } else { // lost both walls
-    forward(20);
-    if (lastSeenWall){ // if it was right wall following 
-      goToAngle(270);
-    } else {
-      goToAngle(90);
-    }
-    forward(20);
-  }
-   }
-  for (int i = 0; i < 5; i++) {
-      if (!running) {
-      Serial.println("collide followWall");
-      return;
-    }
   stepperLeft.runSpeed();
   stepperRight.runSpeed();
-  }
-  
+}
 }
 
 
@@ -493,7 +521,7 @@ void blink(int led, int delaySeconds) {
   Ensures: Moves the robot in an apparently random fashion when viewed. Non-Blocking. It runs between random angles, and random forwards
 */
 void randomWander() {
-  if (!running){
+  if (!running) {
     Serial.println("collideWander");
     return;
   }
@@ -501,7 +529,7 @@ void randomWander() {
   digitalWrite(grnLED, HIGH);
   digitalWrite(redLED, LOW);
   digitalWrite(ylwLED, LOW);
-  
+
   int randomBehavior = random(0, 4);  // chooses random behavior 0, 1, 2, 3
   if (randomBehavior <= 1) {
     double randomAngle = 1.0 * random(0, 361);  // chooses random angle to go to from 0-360 deg
@@ -558,7 +586,7 @@ void runAway() {
   Returns: Float representing the force 
 */
 float x_vector() {
-  float kp = 100; //scaler gain
+  float kp = 100;                                                   //scaler gain
   float front_force = (dist.front > 0) ? (1.0 / dist.front) : 0.0;  //stops dividing by 0 which would be an error
   float back_force = (dist.back > 0) ? (1.0 / dist.back) : 0.0;
   return kp * (front_force - back_force);
@@ -568,8 +596,8 @@ float x_vector() {
   Ensures: Returns the Y component of the vector that points twoards the object it sees
 */
 float y_vector() {
-  float kp = 100;//scaler gain
-  float left_force = (dist.left > 0) ? (1.0 / dist.left) : 0.0; //stops dividing by 0 which would be an error
+  float kp = 100;                                                //scaler gain
+  float left_force = (dist.left > 0) ? (1.0 / dist.left) : 0.0;  //stops dividing by 0 which would be an error
   float right_force = (dist.right > 0) ? (1.0 / dist.right) : 0.0;
   return kp * (left_force - right_force);
 }
@@ -577,47 +605,46 @@ float y_vector() {
 /*
   Ensures: Keep the robot centered while driving between two walls, uses proportional control
 */
-void followCenter(){
+void followCenter() {
 
-  int error = dist.left - dist.right; // If positive: farther to left, If Negavive farther to right
-  
+  int error = dist.left - dist.right;  // If positive: farther to left, If Negavive farther to right
+
   int rightSpeed = 500;
   int leftSpeed = 500;
   int kp = 20;
 
-  if(abs(error) > centerDeadband){
+  if (abs(error) > centerDeadband) {
 
-    if(error > 0){
-      leftSpeed -= kp*error;
-    } else{
-      rightSpeed -= kp*-1*error; //this error would be nagative, don't want to make speed faster by subtracting negative so must multiply -1
+    if (error > 0) {
+      leftSpeed -= kp * error;
+    } else {
+      rightSpeed -= kp * -1 * error;  //this error would be nagative, don't want to make speed faster by subtracting negative so must multiply -1
     }
   }
 
   stepperRight.setSpeed(-1.0 * leftSpeed);
   stepperLeft.setSpeed(-1.0 * rightSpeed);
 
-  for(int i = 0; i < 10; i++){
-    stepperRight.runSpeed();
-    stepperLeft.runSpeed();
+  for (int i = 0; i < 10; i++) {
+    if (running) {
+      stepperRight.runSpeed();
+      stepperLeft.runSpeed();
+    } else {
+      return;
+    }
   }
-
 }
 
 /*
   Ensures: This is the main state logic to switch between follow wall, follow center, and random wander
 */
-void followCenterLogic(){
+void followCenterLogic() {
 
-  if(dist.left != 0 && dist.right != 0){
+  if (dist.left != 0 && dist.right != 0) {
     followCenter();
-  }else if(dist.left != 0 || dist.right != 0){
+  } else if (dist.left != 0 || dist.right != 0) {
     followWall();
-  }else{
+  } else {
     randomWander();
   }
-
-
-
 }
-
