2ac7682a23540d88e538ac91bef5ebae32334afe
rumzissc <rumzissc@rose-hulman.edu>
2026-02-06 11:28:32 -0500

    started bfs algo

---
+++ Final Project/BFSAlgorithm.py
@@ -0,0 +1,50 @@
+# A point in a Maze (Needed for QNode)
+class Point:
+    def __init__(self, x_, y_):
+        self.x = x_
+        self.y = y_
+
+# A QNode (Needed for BFS)
+class QNode:
+    def __init__(self, p_, d_):
+        self.p = p_
+        self.d = d_
+
+def isValid(point):
+    return (0 <= point.x < 4) and (0 <= point.y < 4) # map is 4 tiles, does not include 4
+
+def BFS(mat, start, goal):
+    r = len(mat)
+    c = len(mat[0])
+    
+    # If Source and destination are valid
+    if not mat[start.x][start.y] or not mat[goal.x][goal.y]: return -1
+
+    # Do BFS using Queue and Visited
+    visited = [[False] * c for _ in range(r)]
+    from collections import deque
+    q = deque([QNode(start, 0)])
+    visited[start.x][start.y] = True
+    while q:
+        
+        # Pop an item from queue
+        node = q.popleft()
+        p = node.p
+        d = node.d
+
+        # If we reached the goalination
+        if p.x == goal.x and p.y == goal.y: return d
+        
+        # Try all four adjacent
+        dx = [-1, 0, 0, 1]
+        dy = [0, -1, 1, 0]
+        for i in range(4):
+            nx, ny = p.x + dx[i], p.y + dy[i]
+            if isValid(nx, ny, r, c) and mat[nx][ny] and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append(QNode(Point(nx, ny), d + 1))
+    return -1
+oGrid = [[0, 99, 99, 0], [0, 0, 0, 0], [0, 99, 99, 0], [0, 99, 0, 0]]
+TMap1 = [[9, 5, 5, 3], [14, 15, 15, 10], [15, 9, 5, 6], [13, 6, 15, 15]]
+TMap2 = [[11, 15, 15, 11], [8, 5, 5, 2], [10, 15, 15, 10], [14, 13, 5, 6]]
+print(BFS(oGrid, Point(0, 3), Point(3, 0)))
