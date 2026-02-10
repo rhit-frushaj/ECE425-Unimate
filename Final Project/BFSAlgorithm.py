836b07fa2a316d3984355ef2300db780dc164218
rumzissc <rumzissc@rose-hulman.edu>
2026-02-10 10:19:03 -0500

    working command vector

--- Final Project/BFSAlgorithm.py
+++ Final Project/BFSAlgorithm.py
@@ -58,8 +58,8 @@ def BFS(mat, start, goal):
 
 oGrid = [[0, 99, 99, 0], [0, 0, 0, 0], [0, 99, 99, 0], [0, 99, 0, 0]]
 
-start = Point(0, 3)
-goal = Point(3, 0)
+start = Point(0, 0)
+goal = Point(3, 2)
 distance, path = BFS(oGrid, start, goal)
 
 oGrid[start.x][start.y] = 1
@@ -70,3 +70,56 @@ print(oGrid[2])
 print(oGrid[3])
 print(f"Distance: {distance}")
 print(f"Path: {path}")
+facing = 'u'
+commandVector = [0]*distance
+for i in range(distance):
+    if path[i][0] < path[i+1][0]:
+        if facing == 'u':
+            facing = 'd'
+            commandVector[i] = 'b'
+        elif facing == 'd':
+            commandVector[i] = 'f'
+        elif facing == 'r':
+            facing = 'd'
+            commandVector[i] = 'r'
+        elif facing == 'l':
+            facing = 'd'
+            commandVector[i] = 'l'
+    if path[i][1] < path[i+1][1]:
+        if facing == 'u':
+            facing = 'r'
+            commandVector[i] = 'r'
+        elif facing == 'd':
+            facing = 'r'
+            commandVector[i] = 'l'
+        elif facing == 'r':
+            commandVector[i] = 'f'
+        elif facing == 'l':
+            facing = 'r'
+            commandVector[i] = 'b'
+    if path[i][0] > path[i+1][0]:
+        if facing == 'u':
+            commandVector[i] = 'f'
+        elif facing == 'd':
+            facing = 'u'
+            commandVector[i] = 'b'
+        elif facing == 'r':
+            facing = 'u'
+            commandVector[i] = 'l'
+        elif facing == 'l':
+            facing = 'u'
+            commandVector[i] = 'r'
+    if path[i][1] > path[i+1][1]:
+        if facing == 'u':
+            facing = 'l'
+            commandVector[i] = 'l'
+        elif facing == 'd':
+            facing = 'l'
+            commandVector[i] = 'r'
+        elif facing == 'r':
+            facing = 'l'
+            commandVector[i] = 'b'
+        elif facing == 'l':
+            commandVector[i] = 'f'
+    
+print(f"CommandPath: {commandVector}")
